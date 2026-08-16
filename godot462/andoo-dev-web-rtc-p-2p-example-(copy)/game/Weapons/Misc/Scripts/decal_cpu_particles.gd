59ff7d1b67885faa841468852a6341a4a5794d16
Tarek-BG <ben.gadha.tarek@gmail.com>
2026-08-16 10:36:58 +0100

    clean up

--- godot462/andoo-dev-web-rtc-p-2p-example-(copy)/game/Weapons/Misc/Scripts/decal_cpu_particles.gd
+++
@@ -1,7 +0,0 @@
-extends CPUParticles3D
-
-
-func _ready() -> void:
-	emitting = true
-	await get_tree().create_timer(8.0).timeout
-	get_parent().call_deferred('queue_free')
--- godot462/andoo-dev-web-rtc-p-2p-example-(copy)/game/Weapons/Scripts/WeaponSlotScript.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-class_name WeaponSlotNew
-
-@export var model : Node3D
-@export var weaponId : int
-@export var attackPoint : Marker3D
-@export var muzzleFlashSpawner : Marker3D
